b895cc58d3658f65a98ee276bc7781305ece4d74
Evelyn Hill <evelynplumhill@gmail.com>
2025-05-29 23:09:39 -0400

    init2

---
+++ test/ExampleTest.cs
@@ -0,0 +1,26 @@
+namespace Examples;
+
+using GdUnit4;
+
+using static GdUnit4.Assertions;
+
+
+[TestSuite]
+public class GdUnitExampleTest
+{
+    [TestCase]
+    public void Example()
+    {
+        AssertString("This is an example message")
+          .HasLength(26)
+          .StartsWith("This is an ex");
+    }
+
+    [TestCase]
+    public void Example2()
+    {
+        AssertString("does").HasLength(14);
+    }
+}
+
+
